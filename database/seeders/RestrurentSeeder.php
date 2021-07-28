b035180b3d008924831d1c38ae925d484a851522
Arik Md. Isthiaque <arikbncc@gmail.com>
2021-07-28 21:00:35 +0600

    Update RestrurentSeeder.php
    seeder hidden

--- database/seeders/RestrurentSeeder.php
+++ database/seeders/RestrurentSeeder.php
@@ -16,11 +16,11 @@ class RestrurentSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('restrurents')->insert([
-            'name' => Str::random(6),
-            'phone' => '01' . Str::random(9),
-            'address' => 'dhaka',
-            'manager_id' => '1',
-        ]);
+        // DB::table('restrurents')->insert([
+        //     'name' => Str::random(6),
+        //     'phone' => '01' . Str::random(9),
+        //     'address' => 'dhaka',
+        //     'manager_id' => '1',
+        // ]);
     }
 }
