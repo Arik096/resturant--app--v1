46796566c2cbdbc860c7dd82e652842b331adac2
Arik Md. Isthiaque <arikbncc@gmail.com>
2021-07-28 21:12:54 +0600

    Update RestrurentController.php
    list done

--- app/Http/Controllers/Restrurent/RestrurentController.php
+++ app/Http/Controllers/Restrurent/RestrurentController.php
@@ -5,11 +5,16 @@ namespace App\Http\Controllers\Restrurent;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\View\ViewName;
+use App\Models\Restrurent\Restrurent;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Pagination\Paginator;
 
 class RestrurentController extends Controller
 {
     public function index()
     {
-        return view('retrurent.restrurentList');
+        $restrurents = DB::table('restrurents')
+        ->paginate(10);
+        return view('retrurent.restrurentList', ['restrurents'=>$restrurents]);
     }
 }
