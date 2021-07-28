52cdb5f65d5077c322f9157173a5133f8265a71a
Arik Md. Isthiaque <arikbncc@gmail.com>
2021-07-28 20:19:38 +0600

    Update mainStructure.blade.php
    navbar updated

--- resources/views/mainStructure.blade.php
+++ resources/views/mainStructure.blade.php
@@ -14,60 +14,57 @@
 </head>
 
 <body>
-    <header>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="{{route('RestrurentList')}}">Restro</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                    aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Link</a>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                                data-bs-toggle="dropdown" aria-expanded="false">
-                                Dropdown
-                            </a>
-                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <li><a class="dropdown-item" href="#">Action</a></li>
-                                <li><a class="dropdown-item" href="#">Another action</a></li>
-                                <li>
-                                    <hr class="dropdown-divider">
+    {{-- <div class="continer" style="display: flex; place-content:center"> --}}
+    <div class="continer">
+        <div class="row col-md-12">
+            <header>
+                <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                    <div class="container-fluid">
+                        <a class="navbar-brand" href="{{ route('LandingPage') }}">Restro</a>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                            aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                                <li class="nav-item">
+                                    <a class="nav-link active" aria-current="page"
+                                        href="{{ route('RestrurentList') }}">All Restrurents</a>
                                 </li>
-                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                <li class="nav-item">
+                                    <a class="nav-link active" aria-current="page"
+                                        href="{{ route('RestrurentList') }}">All Restrurents</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link active" aria-current="page"
+                                        href="{{ route('RestrurentList') }}">All Restrurents</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link active" aria-current="page"
+                                        href="{{ route('RestrurentList') }}">All Restrurents</a>
+                                </li>
                             </ul>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-                        </li>
-                    </ul>
-                    <form class="d-flex">
-                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                        <button class="btn btn-outline-success" type="submit">Search</button>
-                    </form>
+                        </div>
+                    </div>
+                </nav>
+            </header>
+
+            <div class="continer">
+                <div class="row" style="display: flex; place-content: center;">
+                    <div class="col-md-10">
+                        @yield('content')
+                    </div>
                 </div>
             </div>
-        </nav>
-    </header>
 
-    @yield('content')
-
-
-
-    <footer class="footer mt-auto py-3 bg-light">
-        <div class="container" style="display: flex; place-content: center;">
-            <span class="text-muted">Copyright @ <a href="https://isthiaque.com/">Arik Md Isthiaque</a></span>
+            <footer class="footer mt-auto py-3 bg-light">
+                <div class="container" style="display: flex; place-content: center;">
+                    <span class="text-muted">Copyright @ <a href="https://isthiaque.com/">Arik Md Isthiaque</a></span>
+                </div>
+            </footer>
         </div>
-    </footer>
-
+    </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
