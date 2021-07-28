9781875c5ae7d53b132da7064856494fb442ddd3
Arik Md. Isthiaque <arikbncc@gmail.com>
2021-07-28 20:43:14 +0600

    Create RestrurentSeeder.php
    table data seeded

---
+++ database/seeders/RestrurentSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Restrurent\Restrurent;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class RestrurentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('restrurents')->insert([
+            'name' => Str::random(6),
+            'phone' => '01' . Str::random(9),
+            'address' => 'dhaka',
+            'manager_id' => '1',
+        ]);
+    }
+}
