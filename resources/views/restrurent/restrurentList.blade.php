e3d596f127e59ae2cda4405476d23c64f5e3a94d
Arik Md. Isthiaque <arikbncc@gmail.com>
2021-07-29 21:11:15 +0600

    Update restrurentList.blade.php
    restaurant destroy done

--- resources/views/restrurent/restrurentList.blade.php
+++ resources/views/restrurent/restrurentList.blade.php
@@ -42,7 +42,7 @@
                                         <td>{{ $restrurent->address }}</td>
                                         <td>{{ $restrurent->manager_id }}</td>
                                         <td>
-                                            <button class="btn btn-primary">
+                                            <button class="btn btn-primary" id="action_buttons">
                                                 <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px"
                                                     fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
                                                     <path
@@ -50,9 +50,9 @@
                                                     <path fill-rule="evenodd"
                                                         d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
                                                 </svg>
-                                                <a href="{{ route('UpRestrurentPage', $restrurent->id) }}">Delete</a>
+                                                <a href="{{ route('UpRestrurentPage', $restrurent->id) }}">Edit</a>
                                             </button>
-                                            <button class="btn btn-danger">
+                                            <button class="btn btn-danger" id="action_buttons">
                                                 <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px"
                                                     fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
                                                     <path
@@ -60,7 +60,7 @@
                                                     <path fill-rule="evenodd"
                                                         d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
                                                 </svg>
-                                                Delete
+                                                <a href="{{ route('DeRestrurent', $restrurent->id) }}">Delete</a>
                                             </button>
                                         </td>
                                         @php
@@ -93,7 +93,7 @@
         max-height: 10px;
     }
 
-    a {
+    #action_buttons a {
         color: whitesmoke !important;
         text-decoration: none !important;
     }
