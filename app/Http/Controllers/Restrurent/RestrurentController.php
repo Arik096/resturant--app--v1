ed966e0e122e2822c84fa35296ad5fcdadf6fe41
Arik Md. Isthiaque <arikbncc@gmail.com>
2021-07-29 20:37:02 +0600

    Update RestrurentController.php
    restaurant Create done

--- app/Http/Controllers/Restrurent/RestrurentController.php
+++ app/Http/Controllers/Restrurent/RestrurentController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\View\ViewName;
 use App\Models\Restrurent\Restrurent;
+use Illuminate\Contracts\Session\Session;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Pagination\Paginator;
 
@@ -14,7 +15,33 @@ class RestrurentController extends Controller
     public function index()
     {
         $restrurents = DB::table('restrurents')
-        ->paginate(10);
-        return view('retrurent.restrurentList', ['restrurents'=>$restrurents]);
+            ->paginate(10);
+        return view('restrurent.restrurentList', ['restrurents' => $restrurents]);
     }
+
+
+    public function createPage()
+    {
+        return view('restrurent.addRestrurent');
+    }
+
+    public function create(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|max:20',
+            'phone' => 'required|min:11',
+            'address'=> 'required',
+            'manager_id'=> 'required|numeric'
+        ]);
+
+        DB::table('restrurents')->insert([
+            'name' => $request->name,
+            'phone' => $request->phone,
+            'address' => $request->address,
+            'manager_id' => $request->manager_id,
+        ]);
+        Session()->flash('sms','Restrurent Added');
+        return redirect(route('RestrurentList'));
+    }
+
 }
